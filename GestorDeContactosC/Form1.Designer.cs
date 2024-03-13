2ada2ab6baa6733c61cb1e1845d5fd3ad1211e28
RuddyDLP <146477398+RuddyDLP@users.noreply.github.com>
2024-03-12 20:02:06 -0400

    ygukb

--- GestorDeContactosC/Form1.Designer.cs
+++ GestorDeContactosC/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            button1.Location = new Point(427, 157);
+            button1.Location = new Point(443, 146);
             button1.Name = "button1";
-            button1.Size = new Size(256, 29);
+            button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
-            button1.Text = "Button11132";
+            button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
             // Form1
